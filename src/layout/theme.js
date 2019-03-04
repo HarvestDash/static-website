8f737b3dc489d2c5fe1e96130fc45c6f79702605
jromest <jromestiller@gmail.com>
2019-03-04 18:45:40 +0800

    add system font stacks as fallback

--- src/layout/theme.js
+++ src/layout/theme.js
@@ -1,7 +1,8 @@
 const theme = {
   global: {
     font: {
-      family: `'Inter UI'`,
+      family: `"Inter UI", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif`,
     },
   },
 };
