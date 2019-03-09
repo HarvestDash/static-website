0d6e83a0edbd7ad276d1f2e7c1f79d5154332161
Drailan John Terrible <drailanjohn.gss@gmail.com>
2019-03-09 20:22:19 +0800

    fix linter issues

--- src/components/ForWhomSection.js
+++ src/components/ForWhomSection.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Box, Heading, Text, Grid } from 'grommet';
-import Container from '../shared-components/Container';
+import { Box, Heading, Grid } from 'grommet';
 
 const ForWhom = () => (
   <Box as="section">
-    <Box
-      justify="center"
-      align="center"
-    >
+    <Box justify="center" align="center">
       <Heading level="2">FarmGoat is for</Heading>
     </Box>
     <Box justify="center" align="center">
@@ -18,8 +13,12 @@ const ForWhom = () => (
         gap="large"
         margin={{ top: '50px' }}
       >
-        <Box background="light-5" align="center" justify="center">Urban Farmers</Box>
-        <Box background="light-2" align="center" justify="center">Buyers</Box>
+        <Box background="light-5" align="center" justify="center">
+          Urban Farmers
+        </Box>
+        <Box background="light-2" align="center" justify="center">
+          Buyers
+        </Box>
       </Grid>
     </Box>
   </Box>
