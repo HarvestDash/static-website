5c4f2d61fc58d83034a286664789721c2d6ba115
jromest <jromestiller@gmail.com>
2019-03-12 00:50:56 +0800

    create sign up buttons as components

---
+++ src/components/SignUpButtons.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button } from 'grommet';
+
+const SignUpButtons = () => (
+  <React.Fragment>
+    <Button
+      href="https://form.responster.com/ukgxQr"
+      primary
+      label="Sign Up as a Farmer"
+      margin="small"
+    />
+    <Button
+      href="https://form.responster.com/IQy9YS"
+      label="Sign Up as a Local Business"
+      margin="small"
+    />
+  </React.Fragment>
+);
+
+export default SignUpButtons;
