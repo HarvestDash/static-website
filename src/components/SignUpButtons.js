8eac0c31a98c839eeab257b06ce63827b4b0f6cf
jromest <jromestiller@gmail.com>
2019-03-12 01:50:43 +0800

    add position props for sign up buttons component

--- src/components/SignUpButtons.js
+++ src/components/SignUpButtons.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Button } from 'grommet';
 
-const SignUpButtons = () => (
+const SignUpButtons = ({ position }) => (
   <React.Fragment>
     <Button
-      href="https://form.responster.com/ukgxQr"
       primary
+      href="https://form.responster.com/ukgxQr"
       label="Sign Up as a Farmer"
-      margin="small"
+      margin={{
+        vertical: 'small',
+        ...(position === 'left' ? { right: 'medium' } : null),
+        ...(position === 'center' ? { horizontal: 'small' } : null),
+      }}
     />
     <Button
       href="https://form.responster.com/IQy9YS"
       label="Sign Up as a Local Business"
-      margin="small"
+      margin={{
+        vertical: 'small',
+        ...(position === 'center' ? { horizontal: 'small' } : null),
+      }}
     />
   </React.Fragment>
 );
+
+SignUpButtons.propTypes = {
+  position: PropTypes.string,
+};
+
+SignUpButtons.defaultProps = {
+  position: '',
+};
 
 export default SignUpButtons;
