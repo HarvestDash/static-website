02b8176a000922dcf5a083fbfbb925e1f6c85581
jromest <jromestiller@gmail.com>
2019-03-14 06:40:39 +0800

    create section shared component

---
+++ src/shared-components/Section.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box } from 'grommet';
+
+const Section = ({ children, as, align, justify, pad, ...rest }) => (
+  <Box
+    as={as || 'section'}
+    align={align || 'center'}
+    justify={justify || 'center'}
+    pad={pad || { vertical: 'large', horizontal: 'large' }}
+    {...rest}
+  >
+    {children}
+  </Box>
+);
+
+Section.propTypes = {
+  children: PropTypes.node.isRequired,
+  as: PropTypes.string,
+  align: PropTypes.string,
+  justify: PropTypes.string,
+  pad: PropTypes.object,
+};
+
+Section.defaultProps = {
+  as: 'section',
+  align: 'center',
+  justify: 'center',
+  pad: { vertical: 'large', horizontal: 'large' },
+};
+
+export default Section;
