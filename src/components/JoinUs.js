728fe4e374f19a3807352560cb2fa2302cbcb616
jromest <jromestiller@gmail.com>
2019-03-18 21:05:52 +0800

    use paragraph as tag and add horizontal margins in paragraph in joinUs section

--- src/components/JoinUs.js
+++ src/components/JoinUs.js
@@ -6,7 +6,12 @@ import Container from '../shared-components/Container';
 const JoinUs = () => (
   <Container align="center" justify="center">
     <Heading level="2">Join Us Today!</Heading>
-    <Text responsive textAlign="center" margin={{ bottom: 'medium' }}>
+    <Text
+      as="p"
+      responsive
+      textAlign="center"
+      margin={{ top: 'none', bottom: 'medium', horizontal: 'medium' }}
+    >
       If you&apos;re an <strong>urban farmer</strong>, interested in marketing
       and selling your produce online or if you&apos;re a{' '}
       <strong>local business</strong>, interested in buying agricultural
