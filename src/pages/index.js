8a74761b46de8036a5e61878eba6001264c226d7
jromest <jromestiller@gmail.com>
2019-03-18 09:21:09 +0800

    add section separator and theme colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,30 +6,57 @@ import ForWhom from '../components/ForWhom';
 import Features from '../components/Features';
 import JoinUs from '../components/JoinUs';
 import Contact from '../components/Contact';
+import {
+  HeroSeparator,
+  TopSeparator,
+  BottomSeparator,
+  FooterSeparator,
+} from '../components/separator';
+import theme from '../layout/theme';
 
-const LandingPage = () => (
-  <React.Fragment>
-    <Layout>
-      <Section pad={{ vertical: 'xlarge', horizontal: 'large' }}>
-        <Hero
-          title="Seeding the Future!"
-          slogan="Connecting urban farmers and local businesses."
-        />
-      </Section>
-      <Section>
-        <ForWhom />
-      </Section>
-      <Section>
-        <Features />
-      </Section>
-      <Section>
-        <JoinUs />
-      </Section>
-      <Section>
-        <Contact />
-      </Section>
-    </Layout>
-  </React.Fragment>
-);
+const LandingPage = () => {
+  const { colors } = theme.global;
+  const accent1 = colors['accent-1'];
+  const accent2 = colors['accent-2'];
 
+  return (
+    <React.Fragment>
+      <Layout>
+        <Section background="lightgray" pad="none">
+          <Section
+            pad={{ top: 'xlarge', bottom: 'large', horizontal: 'large' }}
+          >
+            <Hero
+              title="Seeding the Future!"
+              slogan="Connecting urban farmers and local businesses."
+            />
+          </Section>
+          <HeroSeparator color="#fff" />
+        </Section>
+        <Section>
+          <ForWhom />
+        </Section>
+        <TopSeparator color={accent1} />
+        <Section
+          background="accent-1"
+          margin={{ top: '-6px' }}
+          pad={{ top: 'large', bottom: 'xlarge', horizontal: 'large' }}
+        >
+          <Features />
+        </Section>
+        <Section
+          background="accent-2"
+          pad={{ top: 'xlarge', bottom: 'large', horizontal: 'large' }}
+        >
+          <JoinUs />
+        </Section>
+        <BottomSeparator color={accent2} />
+        <Section pad={{ top: 'large', bottom: 'xlarge', horizontal: 'large' }}>
+          <Contact />
+        </Section>
+        <FooterSeparator color="lightgray" />
+      </Layout>
+    </React.Fragment>
+  );
+};
 export default LandingPage;
