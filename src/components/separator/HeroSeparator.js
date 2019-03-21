a4e3e038a3f991948cd239c8f458b2d20087f4c2
jromest <jromestiller@gmail.com>
2019-03-21 18:32:21 +0800

    use styled svg to all svg separator

--- src/components/separator/HeroSeparator.js
+++ src/components/separator/HeroSeparator.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import StyledSvg from './StyledSvg';
 
 const HeroSeparator = ({ color }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 160" fill={color}>
+  <StyledSvg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 1440 160"
+    fill={color}
+  >
     <g>
       <path
         d="M0,160 L0,0 C548.949848,3.38469e-14 823.425766,157.977 1440,157.977 L1440,160 L0,160 Z"
         transform="matrix(-1 0 0 1 1440 0)"
       />
     </g>
-  </svg>
+  </StyledSvg>
 );
 
 HeroSeparator.propTypes = {
