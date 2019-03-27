047acb5f96c32928289d8baf5b62628581287527
jromest <jromestiller@gmail.com>
2019-03-28 06:11:22 +0800

    change top and bottom separator

--- src/components/separator/BottomSeparator.js
+++ src/components/separator/BottomSeparator.js
@@ -8,7 +8,7 @@ const BottomSeparator = ({ color }) => (
     viewBox="0 0 1440 160"
     fill={color}
   >
-    <polygon points="0,160 0,0 1440,0 " />
+    <polygon points="1440,0 1440,160 0,160 " />
   </StyledSvg>
 );
 
