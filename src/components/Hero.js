9ce1acb8d9278104a26ab247052ec4500a2b05de
jromest <jromestiller@gmail.com>
2019-03-28 14:06:58 +0800

    lazy loading hero background image with gatsby-background-image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { StaticQuery, graphql } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
 import { Box, Heading } from 'grommet';
 import SignUpButtons from './SignUpButtons';
 import Section from '../shared-components/Section';
 import Container from '../shared-components/Container';
 import { HeroSeparator } from './separator';
 
-import HeroImage from '../images/index/hero-img.jpg';
-
 const HeroWrapper = styled(Box)`
   position: relative;
-  background-color: #8f8f8f;
-  background-image: url(${HeroImage});
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
 
   & :before {
     position: absolute;
@@ -39,23 +34,50 @@ const HeroWrapper = styled(Box)`
   }
 `;
 
+const BackgroundSection = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        desktop: file(relativePath: { eq: "index/hero-img.jpg" }) {
+          childImageSharp {
+            fluid(quality: 100, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <BackgroundImage fluid={data.desktop.childImageSharp.fluid}>
+        {children}
+      </BackgroundImage>
+    )}
+  />
+);
+
+BackgroundSection.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
 const Hero = ({ title, slogan }) => (
-  <HeroWrapper pad={{ top: 'xlarge' }}>
-    <Section pad={{ top: 'xlarge', bottom: 'xlarge', horizontal: 'large' }}>
-      <Container>
-        <Heading level="1" margin="none">
-          {title}
-        </Heading>
-        <Heading level="2">{slogan}</Heading>
-        <Box direction="row" align="center" justify="start" wrap>
-          <SignUpButtons position="left" />
-        </Box>
-      </Container>
-    </Section>
-    <Box width="full" margin={{ bottom: '-1px' }}>
-      <HeroSeparator color="#fff" />
-    </Box>
-  </HeroWrapper>
+  <BackgroundSection>
+    <HeroWrapper pad={{ top: 'xlarge' }}>
+      <Section pad={{ top: 'xlarge', bottom: 'xlarge', horizontal: 'large' }}>
+        <Container>
+          <Heading level="1" margin="none">
+            {title}
+          </Heading>
+          <Heading level="2">{slogan}</Heading>
+          <Box direction="row" align="center" justify="start" wrap>
+            <SignUpButtons position="left" />
+          </Box>
+        </Container>
+      </Section>
+      <Box width="full" margin={{ bottom: '-1px' }}>
+        <HeroSeparator color="#fff" />
+      </Box>
+    </HeroWrapper>
+  </BackgroundSection>
 );
 
 Hero.propTypes = {
