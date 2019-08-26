8b09d05575437b0c8c0514104e4b24ae08d796f7
jromest <jromestiller@gmail.com>
2019-08-26 14:22:33 +0800

    add global body styles

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Global, css } from '@emotion/core';
 import { ThemeProvider } from 'emotion-theming';
 
 import 'normalize.css';
@@ -8,9 +9,20 @@ import 'typeface-inter';
 import theme from './theme';
 import SEO from '../components/SEO';
 
+const globalStyles = css`
+  body {
+    font-family: 'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+      Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+    font-weight: 400;
+    line-height: 1.5;
+  }
+`;
+
 const Layout = ({ children }) => (
   <ThemeProvider theme={theme}>
     <SEO />
+    <Global styles={globalStyles} />
     {children}
   </ThemeProvider>
 );
