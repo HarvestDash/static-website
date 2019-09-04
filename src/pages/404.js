744a3c768520b71379dd2673c0af0c48191244b1
jromest <jromestiller@gmail.com>
2019-09-04 23:52:24 +0800

    apply styles in 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Flex, Heading, Text } from 'rebass';
+import Layout from '../layout';
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </div>
+  <Layout>
+    <Flex height="100%" flexDirection="column" justifyContent="center">
+      <Heading as="h1" variant="display">
+        NOT FOUND
+      </Heading>
+      <Text fontSize={[1, 2, 3]}>
+        You just hit a route that doesn&#39;t exist... the sadness.
+      </Text>
+    </Flex>
+  </Layout>
 );
 
 export default NotFoundPage;
