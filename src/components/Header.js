aaaba3ce936cf242e3b57663d2cb4595a19373b4
jromest <jromestiller@gmail.com>
2019-09-04 22:27:05 +0800

    add background color in header on scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import throttle from 'lodash.throttle';
 import { useStaticQuery, graphql } from 'gatsby';
 import { Box, Flex, Text } from 'rebass';
 import Logo from '../images/HarvestDash-Logo-White.svg';
 
 const Header = () => {
+  const [isOnTop, setIsOnTop] = useState(true);
+
+  const handleScroll = throttle(() => {
+    const currentScrollPosition = window.pageYOffset;
+
+    if (currentScrollPosition === 0) {
+      setIsOnTop(true);
+    } else {
+      setIsOnTop(false);
+    }
+  }, 300);
+
+  useEffect(() => {
+    document.addEventListener('scroll', handleScroll);
+
+    return () => {
+      document.removeEventListener('scroll', handleScroll);
+    };
+  });
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -24,7 +45,10 @@ const Header = () => {
         top: 0,
         left: 0,
         right: 0,
+        zIndex: 99,
+        transition: 'background-color 0.3s ease-in-out',
       }}
+      bg={!isOnTop ? 'rgba(0, 0, 0, 0.3)' : ''}
     >
       <Flex px={4} py={3} alignItems="center" variant="container">
         <Box mr={3} width={['40px', '50px']} height="auto">
